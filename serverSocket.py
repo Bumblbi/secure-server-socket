6f6dedc281991003fe3c52f28035ec216afe5052
Bumblbi <svahrusev45@yandex.ru>
2025-01-18 16:36:13 +0500

    changed types

--- serverSocket.py
+++ serverSocket.py
@@ -16,7 +16,7 @@ context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2 # we 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
 	sock.bind(('', port))
 	sock.listen(1)
-	with context.wrap_socket(scok, server_side=True) as ssock:
+	with context.wrap_socket(sock, server_side=True) as ssock:
 		conn, addr = ssock.accept()
 		print(addr)
 		message = conn.recv(1024).decode()
