b52eb714eae5d805008b1fc27d7537da6d67c343
Bumblbi <svahrusev45@yandex.ru>
2025-01-18 16:21:31 +0500

    added comments

--- serverSocket.py
+++ serverSocket.py
@@ -6,12 +6,12 @@ server_key = 'server.key'
 server_cert = 'server.crt'
 port = 8000
 
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.verify_mode = ssl.CERT_REQUIRED
-context.load_verify_locations(cafile-client_cert)
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH) # setting the default context to support client authentication
+context.verify_mode = ssl.CERT_REQUIRED # server verification of client certificates
+context.load_verify_locations(cafile-client_cert) # we specify the location of the client and server certificates
 context.load_cert_chain(certfali=server_cert, keyfile=server_key)
 context.options |= ssl.OP_SINGLE_ECDH_USE
-context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2
+context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2 # we prohibit the use of all previous versions of the TLS protocol
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
 	sock.bind(('', port))
