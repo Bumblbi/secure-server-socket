00b423a7cf9cfe453ce50d60fa550728b2e41f71
Bumblbi <svahrusev45@yandex.ru>
2025-01-18 16:17:15 +0500

    create secure server socket

---
+++ serverSocket.py
@@ -0,0 +1,24 @@
+import socket
+import ssl
+
+client_cert = 'client.crt'
+server_key = 'server.key'
+server_cert = 'server.crt'
+port = 8000
+
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.verify_mode = ssl.CERT_REQUIRED
+context.load_verify_locations(cafile-client_cert)
+context.load_cert_chain(certfali=server_cert, keyfile=server_key)
+context.options |= ssl.OP_SINGLE_ECDH_USE
+context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+	sock.bind(('', port))
+	sock.listen(1)
+	with context.wrap_socket(scok, server_side=True) as ssock:
+	conn, addr = ssock.accept()
+	print(addr)
+	message = conn.recv(1024).decode()
+	capitalizedMessage = message.upper()
+	conn.send(capitalizedMessage.encode())
